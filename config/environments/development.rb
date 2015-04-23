356e843c70889e1383ce9c661225599aeaec7d91
Wen-Tien Chang <ihower@gmail.com>
2015-04-23 10:52:05 +0800

    Add email config

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -14,7 +14,7 @@ Rails.application.configure do
   config.action_controller.perform_caching = false
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
@@ -38,4 +38,12 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+
+#config.action_mailer.delivery_method = :letter_opener
+
+config.action_mailer.delivery_method = :smtp
+
+config.action_mailer.default_url_options = { host: "http://localhost:3000" }
+config.action_mailer.smtp_settings = YAML.load(File.read("#{Rails.root}/config/email.yml"))[Rails.env].symbolize_keys
+
 end
